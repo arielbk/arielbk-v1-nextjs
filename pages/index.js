8bc65ce8eb43249581bf56e654892a1f98721ecf
arielbk <buchwald.kearney@gmail.com>
2018-09-10 23:20:27 +0200

    refactor/biobar functional

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,14 @@
+import Head from 'next/head';
+import Biobar from '../components/Biobar';
+
 export default () => (
-  <h1>This is the beginnings of my page!</h1>
+  <div>
+    <Head>
+      <title>My styled page!!</title>
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous" />
+      <link rel="stylesheet" href="static/styles.css" />
+    </Head>
+    <Biobar />
+
+  </div>
 )
