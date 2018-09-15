62c082e3aa6b575f987dc90fdeab7052921fd204
arielbk <buchwald.kearney@gmail.com>
2018-09-15 12:39:20 +0200

    layout adjustments

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default () => (
       <title>arielbk portfolio</title>
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossOrigin="anonymous" />
       <link rel="stylesheet" href="static/styles.css" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
     <Biobar />
     <Portfolio />
